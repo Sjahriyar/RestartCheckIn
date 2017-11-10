83cf6c6604a60b07e60a056af242e70a2c16429a
Sjahriyar <30406716+Sjahriyar@users.noreply.github.com>
2017-11-10 16:29:45 +0100

    front added

---
+++ front/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import registerServiceWorker from './registerServiceWorker';
+
+import Admin from './components/admin';
+import Add from './components/add';
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path = '/' component = {Admin} />
+      <Route path='/add' component ={Add} />
+    </div>
+  </Router>,
+  document.getElementById('root'));
+// registerServiceWorker();
