585ad7704f7755f1cb1ea69d9aeac9d26082631f
Sjahriyar <30406716+Sjahriyar@users.noreply.github.com>
2017-11-10 20:30:56 +0100

    Bootcamp Page

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -3,7 +3,10 @@ const router = express.Router()
 const { check, validationResult } = require('express-validator/check')
 const { matchedData, sanitize } = require('express-validator/filter')
 
-
+//Show the form
+router.get('/', (req,res)=>{
+  res.render('bootcamp')
+})
 
 //Register New Bootcamp
 router.post('/bootcamp/add', (req,res)=>{
@@ -29,25 +32,7 @@ router.post('/student/add', (req,res)=>{
 })
 
 
-//Get List of all Students
-router.get('/student/all', (req,res)=>{
 
-})
-
-//Get student by id
-router.get('student/:id', (req,res)=>{
-
-})
-
-//Update student by ID
-router.put('student/:id', (req,res)=>{
-
-})
-
-//Delete student by id
-router.delete('student/:id', (req,res)=>{
-
-})
 
 
 module.exports = router
