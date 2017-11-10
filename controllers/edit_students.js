a01d7907a9a1c5d98af88c2b88234acfe7d88118
LiberisIvas <liberis.ivas@gmail.com>
2017-11-10 20:55:44 +0100

    edit table

--- controllers/edit_students.js
+++ controllers/edit_students.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const router = express.Router()
 
+
 router.get('/', (req,res)=>{
+  res.render('edit_students')
+
+  SELECT * FROM checking_system.bootcamp_students;
+
+})
+
+
+
+router.put('/', (req,res)=>{
   res.render('edit_students')
 })
 
