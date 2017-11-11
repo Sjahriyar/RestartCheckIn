a6706945858b5cc0b9a17a1d6d76df6ed71c0727
Sjahriyar <30406716+Sjahriyar@users.noreply.github.com>
2017-11-11 16:05:22 +0100

    Server.js file editet
    BodyParser middleware replaced and added to routes.

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -5,23 +5,24 @@ const { matchedData, sanitize } = require('express-validator/filter')
 
 //Show the form
 router.get('/', (req,res)=>{
+
   res.render('bootcamp')
 })
 
 //Register New Bootcamp
-router.post('/bootcamp/add', (req,res)=>{
-
-    req.getConnection(function(err, connection) {
-      if (err) return next(err);
-
-        let bootcamp = {bootcamp_name: '', start_date: '', end_date: '', bootcamp_cancel: '',slack_link:'', login_id:''}
-        let sql = 'INSERT INTO bootcamp_name SET ?'
-        let query = connection.query(sql,bootcamp,(err,result)=>{
-          if (err) throw err
-          res.send("New Bootcamp Registred Successfully!")
-        })
-
-      });
+router.post('/add', (req,res)=>{
+console.log(req.body.bootcamp_name);
+    // req.getConnection(function(err, connection) {
+    //   if (err) return next(err);
+    //
+    //     let bootcamp = {bootcamp_name: '', start_date: '', end_date: '', bootcamp_cancel: '',slack_link:'', login_id:''}
+    //     let sql = 'INSERT INTO bootcamp_name SET ?'
+    //     let query = connection.query(sql,bootcamp,(err,result)=>{
+    //       if (err) throw err
+    //       res.send("New Bootcamp Registred Successfully!")
+    //     })
+    //
+    //   });
 
     })
 
--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const session = require('express-session')
 const ejs = require('ejs')
 const app = express()
 
-
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(bodyParser.json())
 //Setup View Engine
 app.set('view engine', 'ejs')
 
@@ -27,16 +28,16 @@ var mysql = require('mysql'), // node-mysql module
 
 //Middle-Wares
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+
 app.use(myConnection(mysql, dbOptions, 'single'))
 
 //Route
-app.use('/', require('./controllers/admin'))
-app.use('/bootcamp', require('./controllers/bootcamp'))
-app.use('/students', require('./controllers/students'))
-app.use('/records', require('./controllers/records'))
-app.use('/editstud', require('./controllers/edit_students'))
+app.use('/',urlencodedParser, require('./controllers/admin'))
+app.use('/bootcamp',urlencodedParser, require('./controllers/bootcamp'))
+app.use('/students',urlencodedParser, require('./controllers/students'))
+app.use('/records',urlencodedParser, require('./controllers/records'))
+app.use('/editstud',urlencodedParser, require('./controllers/edit_students'))
+
 //Server Listen to port
 app.listen(process.env.PORT || 4500, ()=>{
   console.log('Server is running on port 4500')
