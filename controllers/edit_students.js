b67d7ac4c93169d3ed6af158b67c65de20481b87
LiberisIvas <liberis.ivas@gmail.com>
2017-11-11 16:29:13 +0100

    in the process

--- controllers/edit_students.js
+++ controllers/edit_students.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 
-router.get('/', (req,res)=>{
-  res.render('edit_students')
-
-  SELECT * FROM checking_system.bootcamp_students;
-
-})
-
+// router.get('/', (req,res)=>{
+//   app.find({MYSQL QUERY SELECT * FROM checking_system.bootcamp_students
+//   } (req,res)=>
+// if (err) throw error
+//   res.render('edit_students', {students: data})
+// )
+// })
 
 
 router.put('/', (req,res)=>{
