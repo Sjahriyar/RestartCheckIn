fbb4a0b290ebd7aa3e1b8dca4279cf513128b9b7
LiberisIvas <liberis.ivas@gmail.com>
2017-11-11 19:14:05 +0100

    Edit students

--- controllers/edit_students.js
+++ controllers/edit_students.js
@@ -1,19 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-
-// router.get('/', (req,res)=>{
-//   app.find({MYSQL QUERY SELECT * FROM checking_system.bootcamp_students
-//   } (req,res)=>
-// if (err) throw error
-//   res.render('edit_students', {students: data})
-// )
-// })
-
-
-router.put('/', (req,res)=>{
-  res.render('edit_students')
+router.get('/', (req,res)=>{
+  req.getConnection(function(err, connection) {
+    if (err) return next (err)
+      let sql = "SELECT * FROM checking_system.bootcamp_students"
+      let query = connection.query(sql, (err, result) => {
+        if (err) throw err
+        res.render('edit_students', {data:result})
+        console.log(result);
+    })
+  })
 })
-
 
 module.exports = router
