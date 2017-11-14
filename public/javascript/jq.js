563b1c442e2e08e5ef0d20598f7a208426edabff
Sjahriyar <30406716+Sjahriyar@users.noreply.github.com>
2017-11-14 19:48:34 +0100

    Add

--- public/javascript/jq.js
+++ public/javascript/jq.js
@@ -2,5 +2,8 @@ $(document).ready(function(){
   $('.btn').on('click', function () {
       $('.form').addClass('form--no');
   });
-
+  $(window).on("load resize ", function() {
+    var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+    $('.tbl-header').css({'padding-right':scrollWidth});
+  }).resize();
    });
