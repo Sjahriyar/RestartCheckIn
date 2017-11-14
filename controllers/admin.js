f2d968b7ee005275fe56d5df0b1a932f8b9cfeab
Sjahriyar <30406716+Sjahriyar@users.noreply.github.com>
2017-11-14 19:07:59 +0100

    Add

---
+++ controllers/admin.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const router = express.Router()
+const { check, validationResult } = require('express-validator/check')
+const { matchedData, sanitize } = require('express-validator/filter')
+
+
+//Render Index
+router.get('/', (req,res)=>{
+  res.render('index')
+})
+
+//Render Admin Profile Edit
+router.get('/profile/:id',(req,res)=>{
+  if (!req.session.admin) {
+    res.redirect('/')
+  }
+  else{
+
+  req.getConnection(function(err, connection) {
+    if (err) return next(err);
+
+      let sql = `SELECT * FROM login WHERE login_id = ${req.params.id}`
+      let query = connection.query(sql,(err,result)=>{
+        if (err) throw err
+          res.render('admin_profile', {info: result})
+      })
+
+    });
+  }
+})
+
+router.post('/profile/edit_admin/:id',(req,res)=>{
+  req.getConnection(function(err, connection) {
+    if (err) return next(err);
+
+      let sql = `UPDATE login SET full_name= '${req.body.full_name}', email = '${req.body.email}', password = '${req.body.password}' WHERE login_id = ${req.params.id}`
+      let query = connection.query(sql,(err,result)=>{
+        if (err) throw err
+        req.flash('info', `Profile Successfully Edited!`)
+        res.redirect('back');
+      })
+
+    });
+})
+
+//Login Panel
+router.post('/login', (req,res)=>{
+  req.getConnection(function(err, connection) {
+    if (err) return next(err)
+
+  let sql = `SELECT full_name,email, password FROM login WHERE email = '${req.body.email}' AND  password = '${req.body.password}'`
+  // `SELECT 1 FROM login WHERE email = '${req.body.email}' and password = '${req.body.password}'`
+  let query = connection.query(sql,(err,results)=>{
+    if (err) throw err;
+    //Grab only full name with loop and store it to session
+    if( results.length > 0){
+      for(i=0;i<results.length ; i++){
+        var name = results[i].full_name
+      }
+      req.session.admin = name
+      res.redirect('/bootcamp')
+    }
+    else {
+      req.flash('danger','User Does not exist!')
+    }
+  });
+})
+})
+
+module.exports = router
---
+++ controllers/bootcamp.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const router = express.Router()
+const { check, validationResult } = require('express-validator/check')
+const { matchedData, sanitize } = require('express-validator/filter')
+
+//Show the form
+router.get('/', (req,res)=>{
+
+  req.getConnection(function(err, connection) {
+    if (err) return next(err)
+      let sql = 'SELECT * FROM bootcamp_name'
+      let query = connection.query(sql,(err,results)=>{
+        if (err) throw err
+        res.render('bootcamp',{data: results})
+      })
+    })
+
+})
+
+//Find A bootcamp for edit
+router.get('/showbtcmp/:id', (req,res)=>{
+  req.getConnection(function(err, connection) {
+    if (err) return next(err);
+
+      let sql = `SELECT * FROM bootcamp_name WHERE bootcamp_id = ${req.params.id}`
+      let query = connection.query(sql,(err,result)=>{
+        if (err) throw err
+        res.render('edit_bootcamp', {data: result})
+      })
+
+    });
+})
+
+//Register New Bootcamp
+router.post('/', (req,res)=>{
+
+    req.getConnection(function(err, connection) {
+      if (err) return next(err);
+
+        let bootcamp = {bootcamp_name: req.body.bootcamp_name, start_date: req.body.start_date, end_date: req.body.end_date, slack_link: req.body.slack_link}
+        let sql = 'INSERT INTO bootcamp_name SET ?'
+        let query = connection.query(sql,bootcamp,(err,result)=>{
+          if (err) throw err
+          req.flash('success', `Bootcamp ${req.body.bootcamp_name} is Successfully Registred`)
+          res.redirect('back');
+        })
+
+      });
+
+    })
+    //Edit the bootcamp
+    router.post('/editbtcmp/:id', (req,res)=>{
+      req.getConnection(function(err, connection) {
+        if (err) return next(err);
+
+          let sql = `UPDATE bootcamp_name SET bootcamp_name= '${req.body.bootcamp_name}', start_date = '${req.body.start_date}', end_date = '${req.body.end_date}', slack_link = '${req.body.slack_link}' WHERE bootcamp_id = ${req.params.id}`
+          let query = connection.query(sql,(err,result)=>{
+            if (err) throw err
+            req.flash('info', `Bootcamp Successfully Edited!`)
+            res.redirect('back');
+          })
+
+        });
+    })
+
+router.get('/signout',(req,res)=>{
+      req.session.destroy();
+      res.redirect('/');
+  });
+
+
+module.exports = router
