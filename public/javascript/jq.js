f2c9be0aca2a3b2e5a5194631243ed853fc50ef3
Sjahriyar <30406716+Sjahriyar@users.noreply.github.com>
2017-11-14 20:11:56 +0100

    bhio

--- public/javascript/jq.js
+++ public/javascript/jq.js
@@ -6,4 +6,10 @@ $(document).ready(function(){
     var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
     $('.tbl-header').css({'padding-right':scrollWidth});
   }).resize();
+
+  n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("today").innerHTML = m + "/" + d + "/" + y;
    });
