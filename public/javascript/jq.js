888db8520338bf43f5e81fcdf2136269ed91e8b6
Sjahriyar <30406716+Sjahriyar@users.noreply.github.com>
2017-11-14 20:46:13 +0100

    merged with ivas

--- public/javascript/jq.js
+++ public/javascript/jq.js
@@ -2,5 +2,14 @@ $(document).ready(function(){
   $('.btn').on('click', function () {
       $('.form').addClass('form--no');
   });
+  $(window).on("load resize ", function() {
+      var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+      $('.tbl-header').css({'padding-right':scrollWidth});
+    }).resize();
 
+    n =  new Date();
+  y = n.getFullYear();
+  m = n.getMonth() + 1;
+  d = n.getDate();
+  document.getElementById("today").innerHTML = m + "/" + d + "/" + y;
    });
