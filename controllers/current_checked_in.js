f6c4c51a4609e80a82b7da4fbed5ed9b2b44349c
misheil <x2020x@yahoo.com>
2017-11-15 18:44:08 +0100

    last update

---
+++ controllers/current_checked_in.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const router = express.Router()
+const { check, validationResult } = require('express-validator/check')
+const { matchedData, sanitize } = require('express-validator/filter')
+
+router.get('/',(req,res)=>{
+  req.getConnection(function(err, connection) {
+    if (err) return next(err)
+    connection.query(`SELECT * FROM checking_system.sign_in_tabel inner join bootcamp_students on  sign_in_tabel.stu_id=bootcamp_students.stu_id where Date(sign_in_date)=Date('2017-11-14');`, function (err, result, fields) {
+         if (err) throw err;
+         res.render('live_checkin', {grab: result})
+      })
+    })
+})
+
+router.get('/live2',(req,res)=>{
+  req.getConnection(function(err, connection) {
+    if (err) return next(err)
+    connection.query(`SELECT * FROM checking_system.sign_in_tabel inner join bootcamp_students on  sign_in_tabel.stu_id=bootcamp_students.stu_id where Date(sign_in_date)=Date('2017-11-14');`, function (err, result, fields) {
+         if (err) throw err;
+         res.render('live_checkin2', {grab: result})
+      })
+    })
+})
+
+router.get('/rfid',(req,res)=>{
+  res.render('rfid')
+})
+router.post('/rfid', (req,res)=>{
+  req.getConnection(function(err, connection) {
+    if (err) return next(err);
+
+    var post  = {card_id: req.body.rfid};
+    var query = connection.query('INSERT INTO sign_in_tabel SET ?', post, function (error, results, fields) {
+    if (error) throw error;
+    res.redirect('back')
+      })
+
+    });
+})
+
+module.exports = router
--- controllers/students.js
+++ controllers/students.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const session = require('express-session')
 const router = express.Router()
-const fileUpload = require('express-fileupload');
+
 var expressValidator = require('express-validator');
 var mysql = require('mysql');
 
@@ -10,9 +10,6 @@ var mysql = require('mysql');
 
 router.get('/', (req,res)=>{
   var hour = 3600000
-req.session.cookie.expires = new Date(Date.now() + hour)
-req.session.cookie.maxAge = hour
- req.session.userIsLogdIn="Mishooo";
 
 
   req.getConnection(function(err, connection) {
@@ -23,17 +20,21 @@ req.session.cookie.maxAge = hour
        connection.query("SELECT * FROM countries", function (err2, result_country, fields) {
             if (err2) throw err2;
 
-  res.render('students',{result : result,result_country : result_country,message : '' , errors : ''})
+  res.render('students',{result : result,result_country : result_country,message : '' , errors : '', currentuser: ''});
+    // res.render('students',{result : result,result_country : result_country,message : '' , errors : '', currentuser: ''})
+  // res.render('main');
 });
 });
 // res.end();
+
 });
+
 });
 
 
 
 router.post('/add', (req,res)=>{
-  console.log("XXXXXXXXXXXXX      "+req.session.userIsLogdIn);
+  // console.log("XXXXXXXXXXXXX      "+req.session.userIsLogdIn);
   req.getConnection(function(err, connection) {
     if (err) return next(err);
 // req.checkBody
@@ -42,11 +43,19 @@ req.checkBody('stu_name', 'Student Name is required').not().isEmpty();
 req.checkBody('stu_birth', 'Birth date is required').not().isEmpty();
 req.checkBody('stu_nath', 'Nationality is required').not().isEmpty();
 
+req.checkBody('stu_phon', 'Phone # is required').not().isEmpty();
+req.checkBody('stu_email', 'Email is required').isEmail();
+req.checkBody('zip', 'Post Code is required').not().isEmpty();
+req.checkBody('stu_address', 'Address is required').not().isEmpty();
+req.checkBody('stu_city', 'City is required').not().isEmpty();
+
+
+
 var errors = req.validationErrors();
     if(errors){
 
       errors.forEach(function( err){
-console.log("bbbbbbbbb    cccccccccccc   "+err.msg)
+// console.log("bbbbbbbbb    cccccccccccc   "+err.msg)
 });
       connection.query("SELECT * FROM bootcamp_name", function (err, result, fields) {
            if (err) throw err;
@@ -83,9 +92,13 @@ if(!errors){
         connection.query("SELECT * FROM countries", function (err2, result_country, fields) {
              if (err2) throw err2;
 
-   res.render('students',{result : result,result_country : result_country,message : message, errors: errors })
+             connection.query("SELECT * FROM bootcamp_students where stu_name= '" +req.body.stu_name+ "' and email='" +req.body.stu_email+ "' order by stu_id desc", function (err, currentuser, fields) {
+                  if (err) throw err;
+// where stu_name= '" +req.body.stu_name+ "' and email='" +req.body.stu_email+ "'
+// console.log(currentuser);
+   res.render('students',{result : result,result_country : result_country,message : message, currentuser: currentuser, errors: errors })
 res.end();
-
+});
    });
  });
 });
@@ -94,6 +107,4 @@ res.end();
 
 // res.end();
 });
-
-
 module.exports = router
