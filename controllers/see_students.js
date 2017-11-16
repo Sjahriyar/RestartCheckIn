46eb98d594b96acbf94d3d61149913d1abd26887
LiberisIvas <liberis.ivas@gmail.com>
2017-11-16 18:16:34 +0100

    18:16

--- controllers/see_students.js
+++ controllers/see_students.js
@@ -69,11 +69,23 @@ router.post('/edit/:id', (req,res)=>{
     let sql = `UPDATE bootcamp_students SET stu_cancel=1  WHERE stu_id = ${req.params.id}`
      connection.query(sql, function (err, result) {
     if (err) throw err;
-   req.flash('info', `student disabled!`)
+   req.flash('info', `student deleted!`)
     res.redirect('back');
   });
   }
   );
+});
+router.post('/stop/:id', (req,res)=>{
+  req.getConnection(function(err, connection) {
+    if (err) return next(err);
+  let sql = `UPDATE bootcamp_students SET stu_stop=1  WHERE stu_id = ${req.params.id}`
+   connection.query(sql, function (err, result) {
+  if (err) throw err;
+ req.flash('info', `student disabled!`)
+  res.redirect('back');
+});
+}
+);
 });
 
    // res.render('rfid');
