060ebc48e2c183e7bea7db44e79a87729b8d1cb8
Sjahriyar <30406716+Sjahriyar@users.noreply.github.com>
2017-11-16 22:36:43 +0100

    Back

--- controllers/current_checked_in.js
+++ controllers/current_checked_in.js
@@ -6,7 +6,7 @@ const { matchedData, sanitize } = require('express-validator/filter')
 router.get('/',(req,res)=>{
   req.getConnection(function(err, connection) {
     if (err) return next(err)
-    connection.query(`SELECT * FROM checking_system.sign_in_tabel inner join bootcamp_students on  sign_in_tabel.stu_id=bootcamp_students.stu_id where Date(sign_in_date)=Date('2017-11-14');`, function (err, result, fields) {
+    connection.query(`SELECT * FROM checking_system.sign_in_tabel inner join bootcamp_students on  sign_in_tabel.stu_id=bootcamp_students.stu_id where Date(sign_in_date)=Date(NOW());`, function (err, result, fields) {
          if (err) throw err;
          res.render('live_checkin', {grab: result})
       })
